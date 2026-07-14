c226cf3e87a5f22199153955c0ae0ea73359fe0a
arshil692-prog <muktars692@gmail.com>
2026-07-14 14:27:17 +0530

    idenitifer info

---
+++ variables.py
@@ -0,0 +1,36 @@
+"""
+
+name = "arshil"
+variable > name = "arshil"
+
+identifier > = name (indenitifer tell the idenitity of variables , funcation ,class, module, or any other obaject in python. )
+
+ex . def greet ():
+        print("hello")
+
+idenitifer > greet 
+
+class student:
+    pass 
+
+ientifer > student 
+
+== rules of naming identifier ==
+
+name = "arshil" > can cantain letters
+student1 = "rahul" > can cantian digit
+age123 = "shaikh" > (tip : cannot stars with num )
+student_name = "arshil" > underscore allowed (spaces not)
+
+====case senstive=====
+python  idenitifer is case senstive 
+
+                 
+                diffrence 
+     keywords               ideitifer 
+reversed words            user define names 
+fixied mining             custem mining
+variables name            variable name
+if , elif , else          age , name , salery 
+
+"""
