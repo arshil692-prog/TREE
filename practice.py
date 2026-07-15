1f1ca7e9e2db8e561ec21c0af6bfeeda24fea87b
arshil692-prog <muktars692@gmail.com>
2026-07-15 22:21:38 +0530

    15-07

---
+++ practice.py
@@ -0,0 +1,17 @@
+name = "arshil"
+print(name.find("s"))
+
+name = "arshil"
+print(name.upper())
+
+name = "ARSHIL"
+print(name.lower())
+
+name = "MADAM"
+print(name[::-1])
+
+name = "hello"
+print(name[::-1])
+
+num = [1,2,3,45,5,6,7,8,9,10]
+print (name[1:9:2])
