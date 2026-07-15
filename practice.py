6bdfeb2b301ef041d60b38b9cb0a9bea26f5ee9f
arshil692-prog <muktars692@gmail.com>
2026-07-15 22:36:09 +0530

    15-07

--- practice.py
+++ practice.py
@@ -1,17 +1,39 @@
+
+
+
+#using find methods 
 name = "arshil"
 print(name.find("s"))
 
+
+
+# ushing upeer case methods 
 name = "arshil"
 print(name.upper())
 
+
+# using lower case methods 
 name = "ARSHIL"
 print(name.lower())
 
+#pallendromin words 
 name = "MADAM"
 print(name[::-1])
 
+
+#revese the string
 name = "hello"
 print(name[::-1])
 
+
+#using start:end:step
 num = [1,2,3,45,5,6,7,8,9,10]
 print (name[1:9:2])
+
+#write a python program to check entered num is even or odd 
+
+num = int (input("Enter the number"))
+if num % 2 == 0:
+    print(num, "even")
+else:
+    print(num,"odd") 
